07208c737aa696fa54499c40b4fda064e84ebac4
Michael Hargiss <mdhargiss@gmail.com>
2016-11-26 11:27:42 -0500

    update web page

--- hello.rb
+++ hello.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
 
 get '/' do
-  'Hello Docker World!'
+  "<html>
+    <body>
+      <div style=\"
+          position: absolute;
+          width: 300px;
+          height: 200px;
+          z-index: 15;
+          top: 50%;
+          left: 50%;
+          margin: -100px 0 0 -150px;
+          background: lightblue;
+          border: 2px solid lightblue;
+          border-radius: 12px;\">
+        <h1 id='test' style=\"
+            text-align: center;
+            color: white;
+            padding: 5 5 5 5px;\">
+          Hello, Docker World!
+          </br>
+          </br>
+          ヾ(＾∇＾)
+        </h1>
+      </div>
+    </body>
+  </html>"
 end
